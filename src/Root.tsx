97db96935cb30f8e357c307e47e17ecbc445a83e
axzyy <99994889+axzyy@users.noreply.github.com>
2026-01-15 15:16:32 +0000

    integrazione api v1.2

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,22 +1,56 @@
-//import { useState, useEffect } from "react";
-//import { Link } from "react-router";
+import { useState, useEffect } from "react";
+import { PokeAPI } from "./api";
 
-export function Root(){
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+const Card = ({ pokemon }: { pokemon: Pokemon }) => {
   return (
-    <div className="flex gap-8">
-      <Card pokemonName="Charizard" />
-      <Card pokemonName="Pikachu" />
+    <div className="bg-black p-2 border border-white ">
+      <h1 className="text-sm text-white capitalize">
+        #{pokemon.id} {pokemon.name}
+      </h1>
+
+      <div className="flex items-center justify-center">
+        <img
+          src={pokemon.image}
+          alt={pokemon.name}
+          className="w-24 h-24"
+        />
+      </div>
     </div>
   );
 };
-  //ciao 
-const Card = ({ pokemonName }: { pokemonName: string }) => {
+
+export function Root() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    PokeAPI.listPokemons(20).then((response) => {
+      const mapped = response.results.map(
+        (p: { name: string }, idx: number) => {
+          const id = idx + 1;
+
+          return {
+            id,
+            name: p.name,
+            image: `https://img.pokemondb.net/artwork/large/${p.name}.jpg`,
+          };
+        }
+      );
+
+      setPokemons(mapped);
+    });
+  }, []);
+
   return (
-    <div className="bg-green-500">
-      <h1 className="text-2xl text-white">{pokemonName}</h1>
-      <div className="flex flex items-center justify-center">
-        <img src="https://via.placeholder.com/200" alt={pokemonName} className="w-48 h-48 object-cover" />
-      </div>
+    <div className="p-6 grid grid-cols-2 md:grid-cols-4 gap-4">
+      {pokemons.map((p) => (
+        <Card key={p.id} pokemon={p} />
+      ))}
     </div>
   );
 }
